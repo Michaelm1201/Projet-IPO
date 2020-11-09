af1eff5ff1fb40c105260e155fc17f096f21bd73
Michael <michael.mota@u-psud.fr>
2020-11-09 17:34:35 +0100

    mise à jour de Frog : ajout constructeurs + implementations getters

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,34 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
+	private Case position;
+	private Direction direction;
+
+	public Frog(Game game){
+		this.game = game;
+		this.direction = Direction.up;
+		this.position = new Case(game.width/2, 0);
+	}
 
 
+	@Override
+	public Case getPosition() {
+		return this.position;
+	}
+
+	@Override
+	public Direction getDirection() {
+		return this.direction;
+	}
+
+	@Override
+	public void move(Direction key) {
+
+	}
 }
